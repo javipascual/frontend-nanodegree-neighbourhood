0fdfae0b28d8c37f3d2fe90b27f737c6c80bf076
Javier Pascual <javipascualg@gmail.com>
2015-10-27 20:38:44 +0100

    markers animations + locations active

--- js/main.js
+++ js/main.js
@@ -5,63 +5,68 @@ function initializeMap() {
 // Overall viewmodel for this screen, along with initial state
 var LocationsViewModel = function() {
 
-  items = [];
-  for (var i = 0; i < locs.length; i++)
-    items.push(locs[i].name);
+  var self = this;
 
-	 this.items = ko.observableArray(items);
-	 this.itemToAdd = ko.observable("");
+  var mapCanvas = document.getElementById('map');
+  var mapOptions = {
+    center: new google.maps.LatLng(52.496673, 13.424570),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  this.map = new google.maps.Map(mapCanvas, mapOptions);
 
-	 var mapCanvas = document.getElementById('map');
-   var mapOptions = {
-     center: new google.maps.LatLng(52.496673, 13.424570),
-     zoom: 14,
-     mapTypeId: google.maps.MapTypeId.ROADMAP
-   }
-   this.map = new google.maps.Map(mapCanvas, mapOptions);
-   this.addLocationsMarkers(locs);
-}
 
-LocationsViewModel.prototype.search = function() {
-	if (this.itemToAdd() != "") {
+  this.search = function() {
+  	if (this.itemToAdd() != "") {
+  	   var term = this.itemToAdd();
+  		 this.itemToAdd("");
+  	}
+  }
 
-			var mapBounds = this.map.getBounds();
-			var URL = "https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID"
-					  "&client_secret=CLIENT_SECRET" +
-					  "&v=20130815" +
-					  "&ll=52.496673, 13.424570" +
-					  "&query=sushi";
+  // add markers to the map at the desired locations
+  this.addLocationsMarkers = function(locations) {
+  	var cont = document.getElementById('locationsList');
 
-			var URL = "http://api.yelp.com/v2/search?term=" + this.itemToAdd() +
-					 "&bounds=" + mapBounds.getSouthWest().lat() +
-					 "," + mapBounds.getSouthWest().lng() +
-					 "," + mapBounds.getNorthEast().lat() +
-					 "," + mapBounds.getNorthEast().lng() +
-					 "oauth_consumer_key=scl6Z7MTtfGd43flrc742Q";
+  	for (var i = 0; i < locations.length; i++) {
+      var l = locations[i];
+      l.isActive = ko.observable(false);
+  		l.marker = new google.maps.Marker({
+  			position: {lat: l.location.coordinate.latitude, lng:l.location.coordinate.longitude},
+  			label: l.name,
+  			map: this.map,
+        parent: l
+  		});
 
-			console.log(URL)
+      l.marker.addListener('mouseover', function() {
+        this.parent.isActive(true);
+      });
 
-			 var eURL = encodeURI(URL);
+      l.marker.addListener('mouseout', function() {
+        this.parent.isActive(false);
+      });
+  	}
+  }
 
-			 this.itemToAdd("");
-	}
-}
+  this.onMouseOver = function(location) {
 
-// add markers to the map at the desired locations
-LocationsViewModel.prototype.addLocationsMarkers = function(locations) {
-	var cont = document.getElementById('locationsList');
+  console.log(location.name + " ON")
+       location.marker.setAnimation(google.maps.Animation.BOUNCE);
+       location.isActive(true);
 
-	for (var i = 0; i < locations.length; i++) {
-		var marker = new google.maps.Marker({
-			position: {lat: locations[i].location.coordinate.latitude, lng: locations[i].location.coordinate.longitude},
-			label: locations[i].name,
-			map: this.map
-		});
-	}
-}
+  }
 
-LocationsViewModel.prototype.onClick = function(location) {
-	console.log("click");
+  this.onMouseOut = function(location) {
+
+  console.log(location.name + " OFF")
+       location.marker.setAnimation(null);
+location.isActive(false);
+  }
+
+  this.addLocationsMarkers(locs);
+
+
+	this.items = ko.observableArray(locs);
+	this.itemToAdd = ko.observable("");
 }
 
 ko.applyBindings(new LocationsViewModel());
