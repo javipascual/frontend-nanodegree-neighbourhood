3ca514836787a77b54d5a486c2d23d64aeb7e9d1
Javier Pascual <javipascualg@gmail.com>
2015-11-08 22:50:18 +0100

    wip

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function initializeMap() {
-
 }
+
 
 // Overall viewmodel for this screen, along with initial state
 var LocationsViewModel = function() {
@@ -14,11 +14,79 @@ var LocationsViewModel = function() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
   this.map = new google.maps.Map(mapCanvas, mapOptions);
+  this.items = ko.observableArray([]);
+  this.filterTerm = ko.observable("");
+  this.infoWindow = new google.maps.InfoWindow({content: "<div id=infowindow></div>"});
 
+  this.generateContentString = function(location) {
+
+    var parameters = {
+      client_id: "OQDK0BHM4AQA3G2DAZTN3ZUUMNRJX4RG2XQHN5WJKF0TUMCT",
+      client_secret:"G0M3ZFUVZOCD1I3P2CMGT4KFFBYK5ZG5TC5EGLVJK3U2RRC0",
+      v:"20130815",
+      ll:"52.496673,13.424570"
+    };
+
+    var settings =  {
+      url: "https://api.foursquare.com/v2/venues/" + location.id,
+      data: parameters,
+      cache: true,
+      dataType: 'jsonp',
+      success: function(results) {
+
+        var cats = results.response.venue.categories.map(function(a) {return a.name;}).join();
+        var url = results.response.venue.url;
+        var price = results.response.venue.attributes.groups[0].summary;
+
+        var txt = "<div>";
+        txt += "<span class='name'>" + results.response.venue.name + "</name>";
+        if (url) txt += "<span class='url'>" + url + "</span>";
+        txt += "<span class='cats'>" + cats + "</span>";
+        txt += "<span class='price'>" + price + "</span>";
+        txt += "</div>";
+        self.infoWindow.setContent(txt);
+        self.infoWindow.open(self.map, location.marker);
+      },
+      error: function() { // TODO console.log("error");
+      }
+    }
+
+    $.ajax(settings);
+  }
+
+  this.retrieveLocations = function() {
+
+    var parameters = {
+      client_id: "OQDK0BHM4AQA3G2DAZTN3ZUUMNRJX4RG2XQHN5WJKF0TUMCT",
+      client_secret:"G0M3ZFUVZOCD1I3P2CMGT4KFFBYK5ZG5TC5EGLVJK3U2RRC0",
+      v:"20130815",
+      ll:"52.496673,13.424570",
+      query:"vegan&restaurant"
+    };
+
+    var settings =  {
+      url: "https://api.foursquare.com/v2/venues/search",
+  	  data: parameters,
+  	  cache: true,
+  	  dataType: 'jsonp',
+  	  success: function(results) {
+                  var locations = results.response.venues;
+                  self.addLocationsMarkers(locations);
+                  for (var i = 0; i < locations.length; i++)
+                	   self.items.push(locations[i]);
+                  self.filteredItems = self.items;
+              },
+  	  error: function() { // TODO console.log("error");
+      }
+  	}
+
+    $.ajax(settings);
+  }
+
+  this.retrieveLocations();
 
   this.filter = function() {
   	if (this.filterTerm() != "") {
-      console.log(this.filterTerm())
   	   var term = this.filterTerm();
   		 this.filterTerm("");
   	}
@@ -32,7 +100,7 @@ var LocationsViewModel = function() {
       var l = locations[i];
       l.isActive = ko.observable(false);
   		l.marker = new google.maps.Marker({
-  			position: {lat: l.location.coordinate.latitude, lng:l.location.coordinate.longitude},
+  			position: {lat: l.location.lat, lng:l.location.lng},
   			label: l.name,
   			map: this.map,
         parent: l
@@ -49,10 +117,9 @@ var LocationsViewModel = function() {
   }
 
   this.onMouseOver = function(location) {
-
-  console.log(location.name + " ON")
     location.marker.setAnimation(google.maps.Animation.BOUNCE);
     location.isActive(true);
+    self.generateContentString(location);
   }
 
   this.onMouseOut = function(location) {
@@ -60,21 +127,17 @@ var LocationsViewModel = function() {
     location.isActive(false);
   }
 
-  this.addLocationsMarkers(locs);
-
-	this.items = ko.observableArray(locs);
-	this.filterTerm = ko.observable("");
 
   // filter the items using the filter text
   this.filteredItems = ko.computed(function() {
-    var filter = this.filterTerm().toLowerCase();
+    var filter = self.filterTerm().toLowerCase();
     if (!filter) {
-      ko.utils.arrayForEach(this.items(), function(item) {
+      ko.utils.arrayForEach(self.items(), function(item) {
         item.marker.setVisible(true);
       });
-      return this.items();
+      return self.items();
     } else {
-        return ko.utils.arrayFilter(this.items(), function(item) {
+        return ko.utils.arrayFilter(self.items(), function(item) {
             const visible = (item.name.toLowerCase().indexOf(filter) === 0);
             item.marker.setVisible(visible);
             return visible;
